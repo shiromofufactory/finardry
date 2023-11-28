2c7290b74fbaa26c0d56941c25a02220e1f5d22b
shiromofufactory <shiromofufactory@gmail.com>
2023-11-28 15:57:51 +0900

    Safariのセーブ制約警告を強化（PWAまたはChromeに誘導)

--- htmltest.sh
+++ htmltest.sh
@@ -3,7 +3,7 @@ pyxel app2html src.pyxapp
 rm -rf public/*
 mv src.html public/index.html
 OLD="<!DOCTYPE html>"
-NEW="<!DOCTYPE html><meta property='og:title' content='Finardry' /><meta property='og:description' content='FC時代のウィザードリィをFC時代のFF風のUIにアレンジした二次創作ゲーム。ブラウザでプレイ可能。' /><meta property='og:image' content='https://finardry.web.app/ogp.jpg' /><meta property='og:type' content='website' /><meta name='viewport' content='width=768' /><link rel='manifest' href='/manifest.json' /><script> if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js')</script>"
+NEW="<!DOCTYPE html><meta charset='UTF-8'><meta property='og:title' content='Finardry' /><meta property='og:description' content='FC時代のウィザードリィをFC時代のFF風のUIにアレンジした二次創作ゲーム。ブラウザでプレイ可能。' /><meta property='og:image' content='https://finardry.web.app/ogp.jpg' /><meta property='og:type' content='website' /><meta name='viewport' content='width=768' /><link rel='manifest' href='/manifest.json' /><script> if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js')</script><script src='/custom.js'></script>"
 OLD_ESCAPED=$(echo "$OLD" | sed 's/[\/&]/\\&/g')
 NEW_ESCAPED=$(echo "$NEW" | sed 's/[\/&]/\\&/g')
 sed -i '' "s/${OLD_ESCAPED}/${NEW_ESCAPED}/g" public/index.html
---
+++ static/custom.js
@@ -0,0 +1,19 @@
+function isSafari() {
+  const userAgent = navigator.userAgent.toLowerCase()
+  return (
+    userAgent.includes("safari") &&
+    !userAgent.includes("chrome") &&
+    !userAgent.includes("chromium") &&
+    !userAgent.includes("edg")
+  )
+}
+
+function isPWA() {
+  return window.matchMedia("(display-mode: standalone)").matches
+}
+
+if (isSafari() && !isPWA()) {
+  alert(
+    "本作をSafariでプレイする場合、7日の間にサイトを再訪しないとセーブデータが消失します。Chromeなど他のブラウザをお使いいただく、もしくはスマートフォンの場合はPWAとして実行（ホーム画面に追加）いただくことでデータの消失を防ぐことができます。"
+  )
+}
